06ba12f4a900b55c0611785493b4ecff3ae16c2f
Natalia Gutierrez <nata1945.gs@gmail.com>
2025-06-14 16:18:58 -0400

    configuracion para render

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://postgres:marceline25@localhost/modelado') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://postgres:123456@db.utmshjkhipwtbdbeaxpe.supabase.co:5432/postgres') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '123456'
 
