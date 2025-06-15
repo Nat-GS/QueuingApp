a1082379b56a1b8c7129c6c955534d74a968d013
Natalia Gutierrez <nata1945.gs@gmail.com>
2025-06-14 21:27:15 -0400

    bd actualizada

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://postgres:123456@db.utmshjkhipwtbdbeaxpe.supabase.co:5432/postgres') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','postgresql://admin:x4RInidnXfZHHxdMF5JDKlbHwhFzr20W@dpg-d1721a8dl3ps739v12hg-a.oregon-postgres.render.com/modelado_qy8z') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '123456'
 
